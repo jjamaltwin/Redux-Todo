ab2dc475749f236001a5a441c4078d515b9f03a1
unknown <jjamal95@gmail.com>
2019-06-20 22:57:06 -0500

    Finished AddTodo.js container

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import './App.css';
+import AddTodo from './containers/AddTodo';
 
-function App() {
-  return (
-    <div className="App">
+const App = () => (
+  <div>
+    <AddTodo />
 
-    </div>
-  );
-}
+  </div>
+)
 
-export default App;
+export default App
